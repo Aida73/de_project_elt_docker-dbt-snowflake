a8ed807f482e9bd5260e4bd5a03cfa23a7d612a7
Aida Sow <sowaida@ept.sn>
2025-04-20 21:47:04 +0200

    add macro to convert celsus to fahrenheit

--- dbt/models/marts/weather_summary.sql
+++ dbt/models/marts/weather_summary.sql
@@ -7,6 +7,11 @@ SELECT
     ROUND(AVG(temperature), 2) AS avg_temp,
     ROUND(MAX(temperature), 2) AS max_temp,
     ROUND(MIN(temperature), 2) AS min_temp,
+    -- Use the macro to convert Celsius to Fahrenheit
+    {{ to_fahrenheit('AVG(temperature)') }} AS avg_temp_f,
+    {{ to_fahrenheit('MAX(temperature)') }} AS max_temp_f,
+    {{ to_fahrenheit('MIN(temperature)') }} AS min_temp_f,
+
     ROUND(AVG(humidity), 2) AS avg_humidity,
     ROUND(AVG(pressure), 2) AS avg_pressure,
     COUNT(*) AS nb_points
